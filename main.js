6b458ed078b22b11fc243d48b94546a9ddceffd2
Oleksandra Kozakevych <olexsashka@gmail.com>
2023-06-07 12:47:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const age = 15;
+console.log(age);
+
+const name = "Sasha";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "I choose to live not just exist.";
+console.log(myString);
+
+let myNumber = 7;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Please enter your name.");
+console.log(userName);
+alert(`Hello`);
+
+console.log(alert("Carefully! Action is dangerous") + confirm("Please confirm the action"));
+
